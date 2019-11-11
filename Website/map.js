33372eaed9e5c28e7f1c86e3d0690520e5dc8228
Andrew T <andrewt25@outlook.com>
2019-11-11 14:05:01 -0800

    Updated map to show nearby restauraunts

--- Website/map.js
+++ Website/map.js
@@ -1,9 +1,43 @@
+
+
 function initMap() {
     // The location of Uluru
     var uluru = {lat: 49.200970, lng: -122.913324};
     // The map, centered at Uluru
-    var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 21, center: uluru, mapTypeId: 'satellite'});
+    var map = new google.maps.Map(document.getElementById('map'), {zoom: 21, center: uluru, mapTypeId: 'satellite'});
+    var infoWindow = new google.maps.InfoWindow({ map: map });
     // The marker, positioned at Uluru
-    var marker = new google.maps.Marker({position: uluru, map: map});
+   // var marker = new google.maps.Marker({position: uluru, map: map});
+    var findPlace = new google.maps.places.PlacesService(map);
+        findPlace.nearbySearch({
+            location : uluru,
+            radius : 300,
+            type : ['restaurant']
+        }, processResult);
+
+    function processResult(results, status)
+    {
+
+        if (status == google.maps.places.PlacesServiceStatus.OK)
+        {
+            for (var i = 0; i < results.length; i++)
+            {
+                placeMarker(results[i])
+            }
+        }
+    }
+
+    function placeMarker(place)
+    {
+        var loc = place.geometry.location;
+        var marker = new google.maps.Marker({
+            position : loc,
+            map : map
+        });
+        google.maps.event.addListener(marker, 'click', function () {
+            infoWindow.setContent(place.name);
+            infoWindow.open(map, this);
+        });
+    }
 }
+
